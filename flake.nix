6fb1103a1dfbae3daad5e5b3f24d115a3fb21bde
Datacom-Ayden <ayden.melhop2@datacom.com>
2025-10-23 11:21:17 +1000

    Add .envrc, flake.lock, and flake.nix for Nix-based DevContainer setup

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "DevContainer development environment for Arduino development using Nix";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      inherit (nixpkgs) lib;
+      forAllSystems = lib.genAttrs lib.systems.flakeExposed;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell rec {
+            # These are tools you want available on your PATH
+            packages = with pkgs; [
+              devcontainer
+            ];
+          };
+        }
+      );
+    };
+}
